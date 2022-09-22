366576e5108c632d9f2efe5283cc2ed0e550c9bc
Oleg <bender987@bk.ru>
2022-09-22 21:48:43 +0300

    Function getAllProduction in farm class

--- src/Farm.php
+++ src/Farm.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace test\src;
+
 class Farm
 {
     public $animals = [];
@@ -15,6 +17,19 @@ class Farm
             }
         }
     }
-}
 
-?>
+    public function getAllProduction($days = 7): void
+    {
+
+        foreach ($this->animals as $name => $obj) {
+            $this->productions[$name] = 0;
+            for ($i = 0; $i < $days; $i++) {
+                for ($j = 0; $j < count($obj); $j++) {
+                    $this->productions[$name] += $obj[$j]->getProduction();
+                }
+            }
+            echo "За неделю " . $name . " Принесла " . $this->productions[$name] . " " . $obj[0]->production;
+            echo PHP_EOL;
+        }
+    }
+}
